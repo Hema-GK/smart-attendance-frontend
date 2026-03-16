755f04f6eeea83a148d1cad2b57228a2287cf7bf
Hema G K <hemahemagk@gmail.com>
2026-03-16 22:16:31 +0530

    'Frontend

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,18 @@
+// import axios from "axios";
+
+// const API = axios.create({
+// // baseURL: "http://10.133.110.113:8000"
+// baseURL: "https://final-production-682d.up.railway.app/"
+
+// });
+
+// export default API;
+
 import axios from "axios";
 
 const API = axios.create({
-// baseURL: "http://10.133.110.113:8000"
-baseURL: "https://final-production-682d.up.railway.app/"
-
+  // This line picks up the Key you just entered in Vercel
+  baseURL: import.meta.env.VITE_API_URL || "https://final-production-682d.up.railway.app/"
 });
 
 export default API;
